4b2c00f5b5f06be0b2b4ce4714246fec7fe282f4
smithrm941 <smithrm941@gmail.com>
2017-09-21 14:11:29 -0700

    profile link in navbar works

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -47,14 +47,6 @@ router.get('/logout', (req, response) => {
 })
 
 router.get('/users/:id', (req, res) => {
-  console.log('inside of id', req.session.user)
-  if (req.session.user) {
-    userId = req.session.user
-    return userProfile(userId)
-    .then((user) => {
-      res.render('profile', {user, edit: false})
-    })
-  }
   userId = req.params.id
   return userProfile(userId)
   .then((user) => {
